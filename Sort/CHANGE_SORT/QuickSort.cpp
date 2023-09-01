af9e4cccf61af339e0d5b47efaf83279feacf2ac
M_B <2812248770@qq.com>
2023-09-01 12:15:16 +0800

    Add swap function

--- Sort/CHANGE_SORT/QuickSort.cpp
+++ Sort/CHANGE_SORT/QuickSort.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int &A, int &B)
+{
+    int tmp = A;
+    A = B;
+    B = tmp;
+}
+
 int Partition(int A[], int low, int high)
 {
     int pivot = A[low]; // 当前表中第一个元素设为枢轴，对表进行划分
@@ -36,6 +43,7 @@ int main()
     int a[] = {7, 3, 5, 9, 12, 2};
     int len = sizeof(a) / sizeof(a[0]);
     QuickSort(a, 0, len - 1);
+    // swap(a[0], a[1]);
     for (int i = 0; i < len; i++)
     {
         printf("%d ", a[i]);
